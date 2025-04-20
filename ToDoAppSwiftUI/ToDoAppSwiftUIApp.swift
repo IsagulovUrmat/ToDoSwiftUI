47f836283c16021ad9dff4a19a6e1170e051722a
isagulovurmat <falone641234@gmail.com>
2025-04-20 12:31:21 +0600

    added gitignore

---
+++ ToDoAppSwiftUI/ToDoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSwiftUIApp.swift
+//  ToDoAppSwiftUI
+//
+//  Created by sunflow on 20/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
