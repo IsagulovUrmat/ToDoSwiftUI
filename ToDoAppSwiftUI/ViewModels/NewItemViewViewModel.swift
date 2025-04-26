e02adac6561222304de21b59462431040f792513
isagulovurmat <falone641234@gmail.com>
2025-04-26 19:07:30 +0600

    Add new items functionality is done

--- ToDoAppSwiftUI/ViewModels/NewItemViewViewModel.swift
+++ ToDoAppSwiftUI/ViewModels/NewItemViewViewModel.swift
@@ -6,8 +6,44 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     
+    @Published var title = ""
+    @Published var dueDate = Date()
+    @Published var showAlert = false
+    
     init() {}
+    
+    func save() {
+        guard canSave else { return }
+        
+        // Get current user ID
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        // Create model
+        let newID = UUID().uuidString
+        let newItem = ToDoListItem(id: newID,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        // Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(newID)
+            .setData(newItem.asDictionary())
+    }
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else { return false}
+        
+        guard dueDate >= Date().addingTimeInterval(-86400) else { return false }
+        
+        return true
+    }
 }
--- ToDoAppSwiftUI/Views/ToDoListView.swift
+++ ToDoAppSwiftUI/Views/ToDoListView.swift
@@ -5,35 +5,40 @@
 //  Created by sunflow on 20/4/25.
 //
 
+import FirebaseFirestore
 import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
+    @FirestoreQuery var items: [ToDoListItem]
     
-    private let userId: String
     
     init(userId: String) {
-        self.userId = userId
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    Text(item.title)
+                }
             }
             .navigationTitle("To Do List")
             .toolbar {
                 Button {
-                    // Action
+                    viewModel.showingNewItemView = true
                 } label: {
                     Image(systemName: "plus")
                 }
-
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView) {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
             }
         }
     }
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "k8moyMXlMCaGTdY2yXz4JcWMV302")
 }
