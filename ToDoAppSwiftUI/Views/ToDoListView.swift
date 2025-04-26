ca3f62701364a5e183e9d245c9a4b6b0804928a9
isagulovurmat <falone641234@gmail.com>
2025-04-26 19:46:35 +0600

    delete items functionality added

--- ToDoAppSwiftUI/Views/ToDoListView.swift
+++ ToDoAppSwiftUI/Views/ToDoListView.swift
@@ -9,20 +9,31 @@ import FirebaseFirestore
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel: ToDoListViewViewModel
     @FirestoreQuery var items: [ToDoListItem]
     
     
     init(userId: String) {
         self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
     
     var body: some View {
         NavigationView {
             VStack {
                 List(items) { item in
-                    Text(item.title)
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                viewModel.delete(id: item.id)
+                            } label: {
+                                Label("Delete", systemImage: "trash") // Используем Label с иконкой
+                            }
+                            
+                        }
                 }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
